d6fd022a9d541395536072c66d773b28a951ac14
megha <kamblemegha93@gmail.com>
2023-02-20 13:05:27 +0530

    commiting for jenkins

---
+++ Addition.java
@@ -0,0 +1,13 @@
+public class Addition {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+	    int add;
+	    add = a+b;
+	    System.out.println("Addition is=" +add);
+	    
+
+	}
+
+}
