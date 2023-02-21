e8d296296a4567d294e4a4c509d28e925396fabb
megha <kamblemegha93@gmail.com>
2023-02-21 08:38:40 +0530

    changes for run periodically

--- Addition.java
+++ Addition.java
@@ -3,8 +3,9 @@ public class Addition {
 	public static void main(String[] args) {
 		int a=10;
 		int b=20;
+		int c=10;
 	    int add;
-	    add = a+b;
+	    add = a+b+c;
 	    System.out.println("Addition is=" +add);
 	    
 
